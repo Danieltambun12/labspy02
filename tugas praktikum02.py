1aa8b8193c7c5e15f5aa242eb266393828ffb46f
Danieltambun12 <56190893+Danieltambun12@users.noreply.github.com>
2019-11-02 15:35:17 +0700

    Add files via upload

---
+++ tugas praktikum02.py
@@ -0,0 +1,15 @@
+a = input("Masukan bilangan A :")
+b = input("Masukan bilangan B :")
+c = input("Masukan bilangan C :")
+
+if a > b and a > c:
+    terbesar = a
+else:
+    if b > c and b > a:
+     Terbesar = b
+    else:
+     terbesar = c
+
+print("jadi bilangan yang terbesar adalah :",terbesar)
+
+
